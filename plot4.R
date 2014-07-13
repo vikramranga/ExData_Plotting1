55732e93a1e2ad8c83e177202242975e08a92df5
vikramranga <vikramranga@users.noreply.github.com>
2014-07-13 11:26:45 +0530

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,48 @@
+########## Plot 4 ##############
+
+h_pow_cons <- read.table("G:/Exploratory Analysis/Week One/exdata_data_household_power_consumption/household_power_consumption.txt", sep = ";", header = TRUE,
+na.string = "?")
+h_pow_cons$Date <- as.Date(h_pow_cons$Date, "%d/%m/%Y")
+
+# plot subset
+
+feb.1n2 <- h_pow_cons[h_pow_cons$Date == as.Date("2007-02-01","%Y-%m-%d") | h_pow_cons$Date == as.Date("2007-02-02","%Y-%m-%d"), ]
+
+
+
+#plot
+png(filename = "G:/Exploratory Analysis/Week One/exdata_data_household_power_consumption/plot4.png", width = 480, height = 480)
+par(mfrow = c(2, 2))
+
+# first one
+with(feb.1n2, plot(Global_active_power, ylab = "Global Active Power (Kilowatts)", xlab = "", main = "", type = "l", axes = F))
+axis(2)
+axis(1, at = c(0, 1440, 2880), labels = c("Thu", "Fri", "Sat"))
+box()
+
+# Second one
+
+with(feb.1n2, plot(Voltage, ylab = "Voltage", xlab = "datetime", main = "", type = "l", axes = F))
+axis(2)
+axis(1, at = c(0, 1440, 2880), labels = c("Thu", "Fri", "Sat"))
+box()
+
+# Third one
+
+with(feb.1n2, plot(Sub_metering_1, ylab = "Energy sub metering", xlab = "", main = "", type = "l", axes = FALSE))
+lines(feb.1n2$Sub_metering_2, col = 'red')
+lines(feb.1n2$Sub_metering_3, col = 'blue')
+axis(2)
+axis(1, at = c(0, 1440, 2880), labels = c("Thu", "Fri", "Sat"))
+legend("topright", col = c("black", "red", "blue"), legend = c("Sub metering 1", "Sub metering 2", "Sub metering 3"), lty = 1)
+box()
+
+# Fourth one
+
+with(feb.1n2, plot(Global_reactive_power, ylab = "Global_reactive_power", xlab = "datetime", main = "", type = "l", axes = F))
+axis(2)
+axis(1, at = c(0, 1440, 2880), labels = c("Thu", "Fri", "Sat"))
+box()
+
+# off to disc
+dev.off()
